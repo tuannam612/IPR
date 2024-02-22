1aed1da071cf1af747a25999e42daa5c6f48195a
tuannam612 <buituannam06122003@gmail.com>
2024-02-22 22:30:36 +0700

    init

--- tutorial1/hello.py
+++ tutorial1/hello.py
@@ -32,7 +32,6 @@ def enhanced_img(image_path, output_path):
     brightness = ImageEnhance.Brightness(image_with_contrast)
     image_with_brightness = brightness.enhance(brightness_factor)
     image_with_brightness.save(output_path)
-    print(f"enhanced image saved as {output_path}")
 enhanced_img("public/flower.jpg", "public/flower_enh.jpg")
 
 def rotate_and_flip_image(image_path, output_path):
@@ -41,16 +40,12 @@ def rotate_and_flip_image(image_path, output_path):
     rotated_image_180 = original_image.rotate(180)
     flipped_horizontal_image = original_image.transpose(Image.FLIP_LEFT_RIGHT)
     flipped_vertical_image = original_image.transpose(Image.FLIP_TOP_BOTTOM)
-    
-    if not os.path.exists(output_path):
-        os.makedirs(output_path)
-    
     rotated_image_90.save(f'{output_path}/rotated_image_90.jpg')
     rotated_image_180.save(f'{output_path}/rotated_image_180.jpg')
     flipped_horizontal_image.save(f'{output_path}/flipped_horizontal_image.jpg')
     flipped_vertical_image.save(f'{output_path}/flipped_vertical_image.jpg')
 
-rotate_and_flip_image("public/flower.jpg", "public/flower_rotate")
+rotate_and_flip_image("public/flower.jpg", "public")
 
 
 def apply_filters(image_path, output_path):
@@ -59,5 +54,4 @@ def apply_filters(image_path, output_path):
     sharpened_image = original_image.filter(ImageFilter.SHARPEN)
     blurred_image.save(f'{output_path}/blurred_image.jpg')
     sharpened_image.save(f'{output_path}/sharpened_image.jpg')
-
-apply_filters("public/flower.jpg", "public/flower_filter.jpg")
+apply_filters("public/flower.jpg", "public")
